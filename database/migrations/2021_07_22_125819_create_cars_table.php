8602dd274f2410fd99367298862d2336ff3eeb69
vladan1996 <vladan.duronjic@gmail.com>
2021-07-22 15:02:16 +0200

    2. Database setting

---
+++ database/migrations/2021_07_22_125819_create_cars_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->id();
+            $table->string('registration_license');
+            $table->string('brand');
+            $table->string('model');
+            $table->date('manufacture_date');
+            $table->text('description')->nullable();
+            $table->bigInteger('category_id')->unsigned();
+            $table->json('properties');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
