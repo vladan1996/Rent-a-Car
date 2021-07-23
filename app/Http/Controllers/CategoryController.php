12d6da49389b7c1185924c4ac366b63ca11211a1
vladan1996 <vladan.duronjic@gmail.com>
2021-07-23 16:39:32 +0200

    4. Make model and controller for categories and and CRUD option coding.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(Category::with('cars')->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $categorys = new Category();
+
+        $categorys->name = $request->input('name');
+        $categorys->price = $request->input('price');
+        $categorys->parent_id = $request->input('parent_id');
+
+        $categorys->save();
+        return response()->json($categorys);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $category = Category::find($id);
+        return response()->json($category);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $category = Category::find($id);
+
+        $category->name = $request->input('name');
+        $category->price = $request->input('price');
+        $category->parent_id = $request->input('parent_id');
+
+        $category->save();
+        return response()->json($category);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $category = Category::find($id);
+        $category->delete();
+        return response()->json($category);
+    }
+}
---
+++ app/Models/Category.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'parent_id'
+    ];
+
+    public function cars()
+    {
+        return $this->hasMany(Car::class);
+    }
+}
